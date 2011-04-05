a01cc81dcb2f444260fa7493275dc3c24be885fa
Attila Zseder <zseder@ilab.sztaki.hu>
2011-04-05 13:49:15 +0200

    nltk tools in one wrapper class - tokenizer - pos-tagger - stemmer

---
+++ Scripts/nltk/nltktools.py
@@ -0,0 +1,126 @@
+import os
+import re
+from collections import defaultdict
+
+from nltk.tokenize.punkt import PunktSentenceTokenizer
+from nltk.tokenize.punkt import PunktWordTokenizer
+from nltk.tag.hunpos import HunposTagger
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from nltk.corpus.reader.wordnet import NOUN, VERB, ADJ, ADV
+
+
+_penn_to_major_pos = {"JJ": ADJ, "JJR": ADJ, "JJS": ADJ,
+                     "NN": NOUN, "NNS": NOUN, "NNP": NOUN, "NNPS": NOUN,
+                     "RB": ADV, "RBR": ADV, "RBS": ADV,
+                     "VB": VERB, "VBD": VERB, "VBG": VERB, "VBN": VERB, "VBP": VERB, "VBZ": VERB}
+penn_to_major_pos = defaultdict(lambda: NOUN)
+penn_to_major_pos.update(_penn_to_major_pos)
+
+class NltkTools:
+    _abbrevPattern = re.compile(r"([\w][\w]?[.]){2}$", re.UNICODE)
+    _cleanerPattern = re.compile("(\w\w)([.?,:;!])(\w)(\w)", re.UNICODE)
+
+    def __init__(self, tok=False, wtok=False, stok=False, pos=False, stem=False,
+                 pos_model=None, abbrev_set=None):
+        """@param abbrev_set: a set of frequent abbreviations."""
+        if tok:
+            wtok = True
+            stok = True
+            self.abbrev_set = (set(abbrev_set) if abbrev_set is not None else set())
+            
+        if wtok:
+            self.wordTokenizer = PunktWordTokenizer()
+            self.punktSplitter = re.compile(r"^([\w\d]+)([.?,:;!])$", re.UNICODE)
+            # Bragantino,2006.In fix this shit
+        if stok:
+            self.senTokenizer = PunktSentenceTokenizer()
+        if pos:
+            if pos_model is not None:
+                self.posTagger = HunposTagger(pos_model, encoding="utf-8")
+            else:
+                self.posTagger = HunposTagger(os.path.join(os.environ['HUNPOS'], 'english.model'), encoding="utf-8")
+        if stem:
+            self.stemmer = WordNetLemmatizer()
+
+    def tokenize(self, raw):
+        sentences = self.sen_tokenize(raw)
+        tokens = [self.word_tokenize(sen) for sen in sentences]
+        for i in reversed(xrange(len(tokens) - 1)):
+            if ( self.is_abbrev(tokens[i][-1])
+                 or NltkTools._abbrevPattern.match(tokens[i][-1]) is not None
+                 and not NltkTools.starts_with_upper(tokens[i + 1][0])):
+                tokens[i].extend(tokens[i + 1])
+                tokens.pop(i + 1)
+        return tokens
+        
+
+    def sen_tokenize(self, raw):
+        raw = NltkTools.cleanup_puncts(raw)
+        return self.senTokenizer.tokenize(raw)
+
+    def sen_abbr_tokenize(self, raw):
+        """Tokenizes the sentence, and tries to handle problems caused by
+        abbreviations and such."""
+        sentences = self.sen_tokenize(raw)
+        for i in reversed(xrange(len(sentences) - 1)):
+            if (NltkTools._abbrevPattern.search(sentences[i]) is not None
+                    and not NltkTools.starts_with_upper(sentences[i + 1])):
+                sentences[i] = ' '.join(sentences[i:i+2])
+                sentences.pop(i + 1)
+        return sentences
+
+    @staticmethod
+    def starts_with_upper(text):
+        """Checks if the sentence starts with an upper case letter."""
+        t = text.lstrip()
+        return len(t) > 0 and t[0].isupper()
+    
+    @staticmethod
+    def cleanup_puncts(raw):
+        pos = 0
+        cleaner = NltkTools._cleanerPattern.search(raw[pos:])
+        while cleaner:
+            if cleaner.group(2) == "." and not cleaner.group(3)[0].isupper():
+                pos = cleaner.end()
+            elif cleaner.group(1)[-1].isdigit() and cleaner.group(3)[0].isdigit():
+                pos = cleaner.end()
+            else:
+                changed_part_string = cleaner.expand(r"\1\2 \3\4")
+                raw = raw[:cleaner.start()] + changed_part_string + raw[cleaner.end():]
+                pos = cleaner.end()
+            cleaner = NltkTools._cleanerPattern.search(raw, pos)
+        return raw
+    
+    def is_abbrev(self, tok):
+        return tok in self.abbrev_set
+
+    def word_tokenize(self, sen):
+        tokens = self.wordTokenizer.tokenize(sen)
+        if len(tokens) == 0:
+            return []
+        punktMatchObject = self.punktSplitter.match(tokens[-1])
+        if punktMatchObject is not None and not self.is_abbrev(tokens[-1]):
+            tokens = tokens[:-1] + list(punktMatchObject.groups())
+        return tokens
+
+    def pos_tag(self, sentokens):
+        return self.posTagger.tag(sentokens)
+
+    def stem(self, tokens):
+        return ((tok, pos, self.stemmer.lemmatize(tok, penn_to_major_pos[pos])) for tok, pos in tokens)
+    
+if __name__ == "__main__":
+    import sys
+    nt = NltkTools(pos=True, stem=True, tok=True)
+    
+    for l in sys.stdin:
+        l = l.decode("utf-8").rstrip()
+        sens = nt.tokenize(l)
+        pos_tagged = list(nt.pos_tag(sen) for sen in sens)
+        stemmed = list(nt.stem(pos_tagged_sen) for pos_tagged_sen in pos_tagged)
+        for sen in stemmed:
+            for tok in sen:
+                print u"\t".join(tok).encode("utf-8")
+            print
+
