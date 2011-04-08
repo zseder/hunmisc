233f89e32b18f061117577a59152c01986ab0268
ndavid <ndavid@info.ilab.sztaki.hu>
2011-04-08 17:28:27 +0200

    Fixed.

--- Scripts/langtools/io/txt_to_conll.py
+++ Scripts/langtools/io/txt_to_conll.py
@@ -1,8 +1,9 @@
 """Converts a text file (optionally with a title) to the conll2 format."""
 
 import os.path
-from langtools import nltk
+from langtools.nltk.nltktools import NltkTools
 from langtools.utils import cmd_utils
+from langtools.utils.file_utils import *
 from langtools.io.conll2.conll_iter import WikiPage
 
 # TODO: rename WikiPage to ConllDocument
@@ -30,34 +31,34 @@ def read_file(infile, title=False):
 def write_doc(doc, outs):
     """Writes the document with to outs. A header line is written, then the
     Title field (if any), then the body."""
-    outs.write("%%#PAGE\t{0}\n".format(doc.title))
+    outs.write(u"%%#PAGE\t{0}\n".format(doc.title))
     if 'Title' in doc.fields:
-        outs.write("%%#Field\tTitle\n")
-        write_text(doc.fields['Title'])
-    outs.write("%%#Field\tBody\n")
-    write_text(doc.fields['Body'])
+        outs.write(u"%%#Field\tTitle\n")
+        write_text(doc.fields['Title'], outs)
+    outs.write(u"%%#Field\tBody\n")
+    write_text(doc.fields['Body'], outs)
             
 def write_text(text, outs):
-    for sen in text:
-        for token in sen:
-            outs.write("\t".join(token))
-            outs.write("\n")
+    for token in text:
+        outs.write(u"\t".join(token))
         outs.write("\n")
 
 if __name__ == '__main__':
     import sys
     
     try:
-        params, args = cmd_utils.get_params_sing(sys.argv[1:], 'i:o:ta', 'i', 0)
+        params, args = cmd_utils.get_params_sing(sys.argv[1:], 'i:o:m:ta', 'i', 0)
         if not os.path.isdir(params['i']):
             raise ValueError('Input must be a directory of files.')
     except ValueError as err:
         print('Error: {0}'.format(err))
-        print(('Usage: {0} -i input_dir [-o output_file] [-t] [-a]').format(
-            sys.argv[0]))
+        print(('Usage: {0} -i input_dir [-o output_file] -m [hunpos_model] ' +
+            '[-t] [-a]').format(sys.argv[0]))
         print('       input_dir: the directory with the input text files.')
+        print('       hunpos_model: the hunpos model file.')
         print('       output_file: the conll2 output file. If omitted, the result will')
         print('                    be written to stdout.')
+        print('       hunpos_model: the hunpos model file.')
         print('       -t: If specified, the first non-empty line of the the text files are')
         print('           considered to be titles, and will be processed accordingly.')
         print('       -a: the output is appended to output_file, instead of overwriting it.')
@@ -65,11 +66,11 @@ if __name__ == '__main__':
     
     if 'o' in params:
         output_mode = 'a' if 'a' in params else 'w'
-        out = open(params['o'], output_mode)
+        out = FileWriter(params['o'], output_mode).open()
     else:
-        out = sys.stdout
+        out = StreamWriter(sys.stdout)
     
-    nt = nltk.NltkTools(pos=True, stem=True, tok=True)
+    nt = NltkTools(pos=True, stem=True, tok=True, pos_model=params.get('m'))
     for infile in filter(os.path.isfile, [os.path.join(params['i'], infile)
                                           for infile in os.listdir(params['i'])]):
         doc = WikiPage(infile)
