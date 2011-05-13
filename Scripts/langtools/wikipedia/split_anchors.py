6ea1df364f54d4532841ab2fd9c183fa27f64a83
ndavid <ndavid@info.ilab.sztaki.hu>
2011-05-13 11:30:29 +0200

    Fixed the code, now it works.

--- Scripts/langtools/wikipedia/split_anchors.py
+++ Scripts/langtools/wikipedia/split_anchors.py
@@ -1,6 +1,7 @@
 ## TODO: merge with anchorize.py!
 import sys
-import cmd_utils
+from langtools.utils import cmd_utils
+from langtools.utils import file_utils
 
 pagesep = "%%#PAGE"
 anchor  = "%%#Field\tAnchor\n"
@@ -8,7 +9,7 @@ anchor  = "%%#Field\tAnchor\n"
 def filter(filter_files):
     bad_pages = set()
     for filter_file in filter_files:
-        fin = FileReader(filter_file).open()
+        fin = file_utils.FileReader(filter_file).open()
         for line in fin:
             bad_pages.add(line.strip())
         fin.close()
@@ -16,33 +17,33 @@ def filter(filter_files):
 
 if __name__ == '__main__':
     try:
-        params, args = cmd_utils.get_params(sys.argv[1:], 'w:t:m:f:n:g:s:', 'wt', 0)
+        params, args = cmd_utils.get_params(sys.argv[1:], 'i:f:', 'if', 0)
     except ValueError as err:
-        sys.stderr.write('Error: {0}'.format(err))
-        sys.stderr.write("Splits the anchor file(s) to two: one that lists the '
-            + 'redirect and disambiguation links, and one that lists the anchors'
-            + ' in the "real" pages. The files should be in utf-8.\n")
+        sys.stderr.write("Error: {0}\n".format(err))
+        sys.stderr.write("Splits the anchor file(s) to two: one that lists the "
+            + "redirect and disambiguation links, and one that lists the anchors"
+            + " in the \"real\" pages. The files should be in utf-8.\n")
         sys.stderr.write("Usage: {0} -i input_file+ -f filter_file+\n")
         sys.stderr.write("       input_file: the input file to split\n")
-        sys.stderr.write("       filter_file: a file that lists redirect or '
-                         + 'disambiguation pages, one page a line\n")
+        sys.stderr.write("       filter_file: a file that lists redirect or "
+                         + "disambiguation pages, one page a line\n")
         sys.exit()
     
     bad_pages = filter(params['f'])
     for infile in params['i']:
-        fin = FileReader(infile).open()
-        good_out = FileWriter(infile + 'good', 'w').open()
-        bad_out = FileWriter(infile + 'bad', 'w').open()
+        fin = file_utils.FileReader(infile).open()
+        good_out = file_utils.FileWriter(infile + 'good', 'w').open()
+        bad_out = file_utils.FileWriter(infile + 'bad', 'w').open()
         curr_out = good_out
         for line in fin:
             l = line.strip()
             if l.startswith(pagesep):
                 page = l[8:]
                 curr_out = bad_out if page in bad_pages else good_out
-                curr_out.write("{0}\n".format(l))
+                curr_out.write(u"{0}\n".format(l))
                 curr_out.write(anchor)
             else:
-                curr_out.write("{0}\n".format(l))
+                curr_out.write(u"{0}\n".format(l))
         good_out.close()
         bad_out.close()
         fin.close()
