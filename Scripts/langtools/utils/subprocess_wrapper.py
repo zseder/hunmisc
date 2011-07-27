0025c6d979c967239cfd3fc41ac66ed17384e198
Attila Zseder <zseder@ilab.sztaki.hu>
2011-07-27 16:46:48 +0200

    wrapper around Popen to use easier

---
+++ Scripts/langtools/utils/subprocess_wrapper.py
@@ -0,0 +1,33 @@
+
+from subprocess import Popen, PIPE
+
+class AbstractSubprocessClass:
+    """
+    Simple abstract wrapper class for commands that need to be
+    called with Popen and communicate with them
+
+    TODO
+    - move into a subprocesshelper file
+    """
+    def __init__(self, runnable, encoding="utf-8"):
+        self._runnable = runnable
+        self._encoding = encoding
+        self._closed = True
+
+    def start(self):
+        self._process = Popen([self._runnable] + self.options,
+                              stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        self._closed = False
+
+    def stop(self):
+        self._process.communicate()
+        self._closed = True
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if not self._closed:
+            self.stop()
+
+    def __del__(self):
+        if not self._closed:
+            self.stop()
+
