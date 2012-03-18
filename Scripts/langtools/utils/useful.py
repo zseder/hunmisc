62ce6887297408e5b89cb49caa02ef16314153c5
David Nemeskey <ndavid@info.ilab.sztaki.hu>
2012-03-18 12:38:54 +0100

    Added useful.py. I *really* need to work on my naming methods...

---
+++ Scripts/langtools/utils/useful.py
@@ -0,0 +1,40 @@
+"""Contains useful miscellaneous methods."""
+
+def all_partitions(lst):
+    """Returns all possible partitionings of a list."""
+    length = len(lst)
+    for splits in xrange(length):
+        for ret in split_list(lst, splits):
+            yield ret
+
+def split_list(lst, splits):
+    """Splits a list all possible ways with the specified number of splits."""
+    if len(lst) <= splits:
+        raise ValueError("length {0} <= splits {1}".format(len(lst), splits))
+
+    if splits == 0:
+        yield [lst]
+    else:
+        for i in xrange(len(lst) - splits, 0, -1):
+            for rest in split_list(lst[i:], splits - 1):
+                ret = [lst[0 : i]]
+                ret.extend(rest)
+                yield ret
+
+def split_length(length, splits):
+    """
+    Splits an integer length all possible ways with the specified number
+    of splits.
+    """
+    if length <= splits:
+        raise ValueError("length {0} <= splits {1}".format(length, splits))
+
+    if splits == 0:
+        yield [length]
+    else:
+        for i in xrange(length - splits, 0, -1):
+            for rest in split_length(length - i, splits - 1):
+                ret = [i]
+                ret.extend(rest)
+                yield ret
+
