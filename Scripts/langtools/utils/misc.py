33aa36abb9ca454b1b9f7a6e725d65ec507509b8
David Nemeskey <ndavid@info.ilab.sztaki.hu>
2012-03-19 18:03:37 +0100

    Added unicode brackets; discovered frozenset.

--- Scripts/langtools/utils/misc.py
+++ Scripts/langtools/utils/misc.py
@@ -9,23 +9,28 @@ def ispunct(s):
     return True
 
 # http://en.wikipedia.org/wiki/Quotation_mark_glyphs.
-__quotationMarks = set(
+quotationMarks = frozenset(
         u'\u0022\u0027\u00AB\u00BB\u2018\u2019\u201A\u201B\u201C\u201D' +
         u'\u201E\u201F\u2039\u203A\u300C\u300D\u300E\u300F\u301D\u301E' +
         u'\u301F\uFE41\uFE42\uFE43\uFE44\uFF02\uFF07\uFF62\uFF63')
+brackets = frozenset(
+        u'\u00AB\u00BB\u2039\u203A\u0028\u0029\u005B\u005D\u003C\u003E' +
+        u'\u007B\u007D\u2308\u2309\u230A\u230B\u27E8\u27E9\u2329\u232A' +
+        u'\uFF62\uFF63\u3008\u3009\u300A\u300B\u300C\u300D\u300E\u300F' +
+        u'\u3010\u3011\uFF08\uFF09\uFF3B\uFF3D\uFF1C\uFF1E\uFF5B\uFF5D')
 # - and & are not quotation marks; nevertheless they have to be removed from the
 # word 
-__otherStickyCharacters = set(u'-&')
-__wikiGarbage = set(u'|[]{}<>()*=')
-__quotationWikiGarbage = __quotationMarks | __otherStickyCharacters | __wikiGarbage
-__empty_set = set()
+otherStickyCharacters = frozenset(u'-&')
+wikiGarbage = frozenset(u'|[]{}<>()*=')
+quotationWikiGarbage = frozenset(quotationMarks | otherStickyCharacters | wikiGarbage)
+empty_set = frozenset()
 
 wikiRemove = set(u'|')
 
 def isquot(s):
     """Returns @c True if all characters in @p s are quotation marks."""
     for c in s:
-        if not c in __quotationMarks:
+        if not c in quotationMarks:
             return False
     return True
 
@@ -33,21 +38,21 @@ def is_quote_or_garbage(s):
     """Returns @c True if all characters in @p s are quotation marks or wiki
     garbage."""
     for c in s:
-        if not c in __quotationWikiGarbage:
+        if not c in quotationWikiGarbage:
             return False
     return True
 
 def remove_quot_from_word(token):
     """Removes quotation marks from the word and returns both the word and
     the removed quotation marks as separate tokens in a list."""
-    return remove_unwanted_characters_from_word(token, __quotationMarks)
+    return remove_unwanted_characters_from_word(token, quotationMarks)
 
 def remove_quot_and_wiki_crap_from_word(token):
     """Removes quotation marks and wiki garbage characters from the word and
     returns both the word and the removed characters as separate tokens in a
     list."""
     return remove_unwanted_characters_from_word(
-            token, __quotationWikiGarbage) #, __wikiRemove)
+            token, quotationWikiGarbage) #, __wikiRemove)
 
 def remove_unwanted_characters_from_word(token, unwanted_set, remove_set=None):
     """Removes the characters @p unwanted_set from around the word and
@@ -57,7 +62,7 @@ def remove_unwanted_characters_from_word(token, unwanted_set, remove_set=None):
         return [token]
     else:
         if remove_set is None:
-            remove_set = __empty_set
+            remove_set = empty_set
         ret, after = [], []
         begin, end = 0, 0
         for i in xrange(0, len(token)):
