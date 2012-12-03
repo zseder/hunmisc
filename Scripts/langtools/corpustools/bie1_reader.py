e0ec4eae5124f2a4843060c87edba5a41ce409ea
Attila Zseder <zseder@ilab.sztaki.hu>
2012-12-03 19:03:58 +0100

    modified:   bie1_reader.py - out of chunks are now tuples of the same length as chunks are, but   have "O" there, at last field

--- Scripts/langtools/corpustools/bie1_reader.py
+++ Scripts/langtools/corpustools/bie1_reader.py
@@ -32,10 +32,10 @@ def read_bie1_sentence(sentence, chunk_field):
 
         # no-chunk token is simple
         if tok[chunk_field] == "O":
-            result.append(token_if_append)
+            result.append(token_if_append + "O")
         # 1-chunks are gonna be one-length chunks
         elif tok[chunk_field].startswith("1-"):
-            result.append([[token_if_append], tok[chunk_field][2:]])
+            result.append(([token_if_append], tok[chunk_field][2:]))
         elif tok[chunk_field][0] == "B":
             active_chunk = [[], tok[chunk_field][2:]]
 
