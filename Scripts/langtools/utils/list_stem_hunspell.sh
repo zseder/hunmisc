cfb0a94d832f80a18f5e95031600fb36081dd499
Katalin Pajkossy <pajkossy@info.ilab.sztaki.hu>
2013-05-23 16:18:59 +0200

    now a new argument is a file containing hunspell paths 	modified:   list_stem_hunspell.sh

--- Scripts/langtools/utils/list_stem_hunspell.sh
+++ Scripts/langtools/utils/list_stem_hunspell.sh
@@ -2,12 +2,16 @@
 language=$1
 input_file=$2
 output_file=$3
+hunspell_path_infos=$4
+
 hunspell=/home/zseder/.local/bin/hunspell
 if [ ! -e $hunspell ];
 then
     echo "needs latest hunspell, currently at nessi6:$hunspell"
     exit
 fi
+
+hunspell_path=`egrep $language $hunspell_path_infos | cut -f2` 
 
 if [[ ${language} == "Bulgarian" ]];
 then
@@ -18,7 +22,7 @@ then
     first_column=`mktemp`
     second_column=`mktemp`
 
-    cat ${input_file} | iconv -f ${input_encoding} -t ${output_encoding} -c | iconv -f ${output_encoding} -t ${input_encoding} | $hunspell -d /mnt/store/home/hlt/Language/Multi/hunspell_dicts/${language}/${language} -s -i ${input_encoding} | egrep ' ' > ${hunspell_output}
+    cat ${input_file} | iconv -f ${input_encoding} -t ${output_encoding} -c | iconv -f ${output_encoding} -t ${input_encoding} | $hunspell -d ${hunspell_path} -s -i ${input_encoding} | egrep ' ' > ${hunspell_output}
     cut -f1 -d' ' ${hunspell_output} > ${first_column}
     cut -f2 -d' ' ${hunspell_output} |  iconv -f ${output_encoding} -t ${input_encoding} > ${second_column}
     paste ${first_column} ${second_column} | sed 's/ /\t/g' > ${output_file}
@@ -26,11 +30,11 @@ then
 
 elif [[ ${language} == "Vietnamese" || ${language} == "Korean" ]];
 then
-	celkod=`grep "^SET" /mnt/store/home/hlt/Language/Multi/hunspell_dicts/${language}/${language}.aff | head -1 | cut -f2 -d" " | sed 's/\r$//'`
-	cat ${input_file} | iconv -t ${celkod} -f utf-8 -c | $hunspell -d /mnt/store/home/hlt/Language/Multi/hunspell_dicts/${language}/${language} -s  | egrep ' ' > ${output_file}
+	celkod=`grep "^SET" ${hunspell_path}.aff | head -1 | cut -f2 -d" " | sed 's/\r$//'`
+	cat ${input_file} | iconv -t ${celkod} -f utf-8 -c | $hunspell -d ${hunspell_path}  -s  | egrep ' ' > ${output_file}
 
 else	
-	celkod=`grep "^SET" /mnt/store/home/hlt/Language/Multi/hunspell_dicts/${language}/${language}.aff | head -1 | cut -f2 -d" " | sed 's/\r$//'`
-	cat ${input_file} | iconv -t ${celkod} -f utf-8 -c | $hunspell -d /mnt/store/home/hlt/Language/Multi/hunspell_dicts/${language}/${language} -s -i ${celkod} 2>${language}.hunspell.err | egrep ' ' | iconv -f ${celkod} -t utf-8 > ${output_file}
+	celkod=`grep "^SET" ${hunspell_path}.aff | head -1 | cut -f2 -d" " | sed 's/\r$//'`
+	cat ${input_file} | iconv -t ${celkod} -f utf-8 -c | $hunspell -d ${hunspell_path} -s -i ${celkod} 2>${language}.hunspell.err | egrep ' ' | iconv -f ${celkod} -t utf-8 > ${output_file}
 fi
 
