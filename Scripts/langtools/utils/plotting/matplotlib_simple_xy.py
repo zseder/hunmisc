7000640ca1216037ca9b79c2b7b6a9391e15cbad
zseder <zseder@ilab.sztaki.hu>
2013-06-04 14:37:54 +0200

    modified:   matplotlib_simple_xy.py to bold

--- Scripts/langtools/utils/plotting/matplotlib_simple_xy.py
+++ Scripts/langtools/utils/plotting/matplotlib_simple_xy.py
@@ -1,6 +1,7 @@
 import sys
 
 import matplotlib.pyplot as plt
+from matplotlib import rc
 
 def read_data(istream):
     r = [[],[],[],[],[]]
@@ -11,11 +12,12 @@ def read_data(istream):
 
 def main():
     d = read_data(open(sys.argv[1]))
+    rc('font', size=14)
     ax = plt.subplot(111)
-    ax.plot(d[0], d[1], label="$M$")
-    ax.plot(d[0], d[2], label="$l KL$")
-    ax.plot(d[0], d[3], label="$l (H_q+KL)$")
-    ax.plot(d[0], d[4], label="$M + l (H_q+KL)$")
+    ax.plot(d[0], d[1], label="$M$", linewidth=2)
+    ax.plot(d[0], d[2], label="$l KL$", linewidth=2)
+    ax.plot(d[0], d[3], label="$l (H_q+KL)$", linewidth=2)
+    ax.plot(d[0], d[4], label="$M + l (H_q+KL)$", linewidth=2)
     plt.xlabel("Bits")
     ax.legend(loc=7)
     plt.show()
