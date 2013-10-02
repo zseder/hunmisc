15b9d42497403cb32ea6cc0cd4278c4255a881da
zseder <zseder@ilab.sztaki.hu>
2013-10-02 14:01:00 +0200

    modified:   prefix_dawg.py - prefix counting moved to another method

--- db/prefix_dawg.py
+++ db/prefix_dawg.py
@@ -3,17 +3,21 @@ from collections import defaultdict
 import dawg
 import marisa_trie
 
+def count_prefixes(freqdawg):
+    d = defaultdict(dict)
+    for full in freqdawg.keys():
+        spl = full.split(" ")
+        for length in xrange(len(spl), 0, -1):
+            s = " ".join(spl[:length])
+            d[s][full] = freqdawg[full]
+    return dict(d)
+
 class PrefixDawg(object):
     def __init__(self, compdawg):
         self.create_from_other(compdawg)
 
     def create_from_other(self, compdawg):
-        d = defaultdict(set)
-        for full in compdawg.keys():
-            spl = full.split(" ")
-            for length in xrange(len(spl), 0, -1):
-                s = " ".join(spl[:length])
-                d[s].add(full)
+        d = count_prefixes(compdawg)
         d = dict([(k, v.pop()) for k, v in d.iteritems() if len(v) == 1])
 
         self.trie = marisa_trie.BytesTrie(((k, v.encode("utf-8"))
