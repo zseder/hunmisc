dbfecdf24abfe45b8687113c1d727ac2160e73a7
zseder <zseder@ilab.sztaki.hu>
2013-10-08 14:51:19 +0200

    modified:   cache.py LangTypeCache for lang-type pair data

--- db/cache.py
+++ db/cache.py
@@ -1,8 +1,10 @@
 import sys
 
 def init_cache(name):
-    # later this will be different for specific caches if needed
-    return DictValueCache()
+    if name in ["freebase", "dbpedia"]:
+        return LangTypeCache()
+    else:
+        return DictValueCache()
 
 class DictValueCache(object):
     """This class (and children) implements a store, and supplementary data
@@ -30,6 +32,32 @@ class DictValueCache(object):
             sys.stderr.write("cache is not finalized yet\n")
             return
         return self.cache[index]
+
+class LangTypeCache(DictValueCache):
+    def __init__(self):
+        self.lang_cache = {}
+        self.type_cache = {}
+
+    def store(self, data):
+        if type(data) != tuple and len(data) != 2:
+            sys.stderr.write("LangTypeCache accepts 2-tuple data\n")
+            return
+
+        lang, type_ = data
+        if lang not in self.lang_cache:
+            self.lang_cache[lang] = len(self.lang_cache)
+        if type_ not in self.type_cache:
+            self.type_cache[type_] = len(self.type_cache)
+
+        DictValueCache.store(self, 
+            (self.lang_cache[lang], self.type_cache[type_]))
+
+    def finalize(self):
+        DictValueCache.finalize(self)
+        self.lang_cache = [v[0] for v in 
+            sorted(lambda x: x[1], self.lang_cache.iteritems(), reverse=True)]
+        self.type_cache = [v[0] for v in 
+            sorted(lambda x: x[1], self.type_cache.iteritems(), reverse=True)]
 
 
 
