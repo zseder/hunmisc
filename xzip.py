81b9a6619a208d68431aad5017c5cec881437468
zseder <zseder@ilab.sztaki.hu>
2013-10-09 10:16:09 +0200

    new file:   xzip.py xzip.gzip_open() is much faster than gzip.open()

---
+++ xzip.py
@@ -0,0 +1,24 @@
+import io
+import os
+import subprocess
+import tempfile
+
+def gzip_open(filename):
+    tmpdir = tempfile.mkdtemp()
+    tmp_fifo = os.path.join(tmpdir, 'myfifo')
+
+    os.mkfifo(tmp_fifo)
+
+    p = subprocess.Popen("gzip --stdout -d %s > %s" % (filename, tmp_fifo), shell=True)
+    f = io.open(tmp_fifo, "r", encoding="utf-8")
+
+    while True:
+        line = f.readline()
+        yield line
+        if not line: break
+     
+    f.close()
+    p.wait()
+     
+    os.remove(tmp_fifo)
+    os.rmdir(tmpdir)
