a8e26223612be992087b08a66cc12caf52415e47
Attila Zseder <attila@cliqz.com>
2013-11-08 17:36:21 +0100

    modified:   matplotlib_pie.py will be good sample for pie plotting

--- utils/plotting/matplotlib_pie.py
+++ utils/plotting/matplotlib_pie.py
@@ -31,25 +31,51 @@ def read_data(istream):
     d = {}
     for l in istream:
         le = l.strip().split()
-        d[le[0]] = [float(_) for _ in le[1:]]
+        d[le[0]] = [_ for _ in le[1:]]
     return d
+
+def classes_pct(s):
+    if float(s) > 1.0:
+        return str(int(round(float(s)))) + "%"
+    else:
+        return ''
+
+def c_pct(s):
+    if float(s) > 10.0:
+        return str(int(round(float(s)))) + "%"
+    elif float(s) > 2.0:
+        return '%1.1f%%' % s
+    else:
+        return ''
 
 def main():
     data = read_data(open(sys.argv[1]))
-    #ax.scatter([_[0] for _ in data.itervalues()],
-            #[_[1] for _ in data.itervalues()],
-            #c='r')
-    data = sorted([(k, v[0]) for k, v in data.iteritems()], key=lambda x: x[1], reverse=True)[:20]
-    labels = [k for k, v in data]
-    sizes = [v for k, v in data]
-    sumsizes = sum(sizes)
+    data = [(k, v[0], int(v[1])) for k, v in data.iteritems()]
+
+    #data = sorted([v for v in data if v[1] == "c"], key=lambda x: x[2])
+    #labels = [k for k, _, _ in data]
+    #sizes = [v for _, _, v in data]
+
+    sum_per_classes = {}
+    for k, cl, l1 in data:
+        if cl == "m": cl = "s"
+        if cl == "c": cl = "t"
+        sum_per_classes[cl] = sum_per_classes.get(cl, 0) + int(l1)
+    labels, sizes = zip(*sorted(sum_per_classes.items(), key=lambda x: x[1]))
+
+    sumsizes = float(sum(sizes))
     relsizes = [v / sumsizes for v in sizes]
+
     explode = [0.0 for _ in sizes]
-    plt.pie(relsizes, explode=explode, labels=labels, autopct='%1.1f%%', startangle=90)
+    colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
+    plt.pie(relsizes, explode=explode, labels=labels, autopct=classes_pct, startangle=90, colors=colors)
+    labels = [(labels[li] if relsizes[li] > 0.02 else "") for li in xrange(len(labels))]
+    #plt.pie(relsizes, explode=explode, labels=labels, autopct=c_pct, startangle=90, colors=colors, pctdistance=0.8, labeldistance=1.05)
     # Set aspect ratio to be equal so that pie is drawn as a circle.
     plt.axis('equal')
     
-    plt.savefig("fig.png", dpi=150, bbox_inches="tight")
+    plt.show()
+    #plt.savefig("fig.png", dpi=150, bbox_inches="tight")
 
 if __name__ == "__main__":
     main()
