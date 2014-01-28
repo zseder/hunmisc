efbb790d12510c0801ba3c6b1669fa7a1f1cc8c3
zseder <zseder@zsedermac.nytud.hu>
2014-01-28 13:44:22 +0100

    modified:   setup.py liblinear building now in installed dir

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 from subprocess import call
 
-call(['cd hunmisc/liblinear; bash install.sh'], shell=True)
+from setuptools.command.install import install
+
+class HunmiscInstall(install):
+    def run(self):
+        install.run(self)
+        cwd = self.install_libbase
+        call(['cd {0}/hunmisc/liblinear; bash install.sh'.format(cwd)], shell=True)
 
 setup(
     author='Attila Zseder',
@@ -10,8 +16,10 @@ setup(
     provides=['hunmisc'],
     url='https://github.com/zseder/hunmisc',
     packages=['hunmisc', 'hunmisc.db', 'hunmisc.liblinear', 'hunmisc.xstring'],
+    scripts=["hunmisc/liblinear/install.sh", "hunmisc/liblinear/liblinear.patch"],
     package_dir={'': '.'},
     include_package_data=True,
     zip_safe=False,
     platforms='any',
+    cmdclass={'install': HunmiscInstall}
 )
