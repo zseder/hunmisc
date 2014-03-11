7219ed26d590e00affeccd458cb943b720239d17
Dávid <david@cliqz.com>
2014-03-11 09:11:34 +0100

    Resolves #4.

--- hunmisc/xstring/stringdiff.py
+++ hunmisc/xstring/stringdiff.py
@@ -105,9 +105,9 @@ def levenshtein(s1, s2, case_sensitive=0, max_distance=sys.maxint,
 	m = len(s1)
 	n = len(s2)
 	if m == 0:
-		return n
+		return n * w_insert
 	if n == 0:
-		return m
+		return m * w_delete
 	# There is a small, but significant difference in performance depending on which 
 	# string is longer, especially for very long strings. Swap the order of the strings 
 	# so that s1 is the shorter.
@@ -154,6 +154,115 @@ def levenshtein(s1, s2, case_sensitive=0, max_distance=sys.maxint,
 
 
 
+class LevenshteinCustomWeights(object):
+	"""
+	Levenshtein edit distance between two strings with custom weights.
+	
+	Note: the only reason why this is in a separate class instead of a function
+	is to avoid the costly inversion of the replacement mapping. Alternatively,
+	we could just not swap the two string -- it would be worth seeing if the
+	performance really decreased; I doubt it (provided we use numpy arrays
+	instead of nested lists).
+	"""
+
+	def __init__(self,
+				 w_insert=1, w_delete=1, w_replace=1,
+				 insert_map={}, delete_map={}, replace_map={}):
+		"""
+		This class differs from the function above in that the user can
+		supply a dictionary for each operation with a {character: cost} mapping
+		for insertion and deletion, and a {character x character: cost} mapping
+		for replacement, thereby customizing the relations.
+
+		The rest of the parameters are:
+		- w_insert: weight of the insertion operator. The default is 1.
+		- w_delete: weight of the deletion operator. The default is 1.
+		- w_replace: weight of the replacement operator. The default is 1 (!).
+		"""
+		self.w_insert = w_insert
+		self.w_delete = w_delete
+		self.w_replace = w_replace
+		self.insert_map = insert_map
+		self.delete_map = delete_map
+		self.replace_map = replace_map
+		self.inv_replace_map = dict(((k[1], k[0]), v)
+									for k, v in replace_map.iteritems())
+
+	def levenshtein(self, s1, s2, case_sensitive=0, max_distance=float('inf')):
+		"""
+		s1 and s2 should be two arbitrary length strings. Returns an integer
+		count of the edit distance between the strings, that is, the number of
+		insertions, deletions and substitutions needed to change one string into
+		the other. A Levenshtein distance of zero implies the strings are the
+		same; the higher the distance, the more different the strings are.
+
+		If case_sensitive is false (the default), comparisons are case
+		insensitive. Case sensitive comparisons only consider standard ASCII
+		upper/lower case and are not internationalised.
+
+		If max_distance is specified, the function returns as soon as the
+		distance reaches this value.
+		"""
+		m = len(s1)
+		n = len(s2)
+		if m == 0:
+			return sum(self.insert_map.get(s2[i], self.w_insert) for i in xrange(n))
+		if n == 0:
+			return sum(self.delete_map.get(s1[i], self.w_delete) for i in xrange(m))
+		# There is a small, but significant difference in performance depending
+		# on which string is longer, especially for very long strings. Swap the
+		# order of the strings so that s1 is the shorter.
+		if n < m:
+			s1, s2 = s2, s1
+			m, n = n, m
+			w_insert, w_delete = self.w_delete, self.w_insert
+			insert_map, delete_map = self.delete_map, self.insert_map
+			replace_map = self.inv_replace_map
+		else:
+			w_insert, w_delete = self.w_insert, self.w_delete
+			insert_map, delete_map = self.insert_map, self.delete_map
+			replace_map = self.replace_map
+
+		# Adjust for case sensitivity.
+		if not case_sensitive:
+			s1 = s1.upper()
+			s2 = s2.upper()
+		# Create an array with rows 0..m and columns 0..n.
+		# We represent the array as a list of lists:
+		#     array = [ row0, row1, row2, ... ]
+		# where each row is also a list.
+		# To access the item in row A column B, use array[A][B].
+		array = [None]*(m+1)
+		for i in range(m+1):
+			array[i] = [None]*(n+1)
+		# Initialise the first row to 0..n.
+		array[0][0] = 0
+		for i in range(1, n+1):
+			array[0][i] = array[0][i - 1] + insert_map.get(s2[i - 1], w_insert)
+		# Initialize the first column to 0..m.
+		for i in range(1, m+1):
+			array[i][0] = array[i - 1][0] + delete_map.get(s1[i - 1], w_delete)
+
+		# Measure the differences.
+		# Loop over the rows and columns of the array, skipping the first of each.
+		# Remember that rows = 0..m and columns = 0..n.
+		for row in range(1, m+1):
+			c1 = s1[row - 1]
+			for col in range(1, n+1):
+				c2 = s2[col - 1]
+				# If the characters are the same, the cost is 0, otherwise it is 1.
+				cost = 0 if c1 == c2 else replace_map.get((c1, c2), self.w_replace)
+				x = array[row-1][col] + delete_map.get(c1, self.w_delete)  # Cell immediately above plus one.
+				y = array[row][col-1] + insert_map.get(c2, self.w_insert)  # Cell immediately to the left plus one.
+				z = array[row-1][col-1] + cost  # Cell diagonally above and to the left, plus the cost.
+				array[row][col] = min(x, y, z)
+				if array[row][col] >= max_distance:
+					return array[row][col]
+		# When done, the bottom-right cell contains the Levenshtein distance.
+		return array[-1][-1]
+
+
+
 def verify():
 	print "Verifying Hamming distance for equal-length strings:"
 	assert hamming("SUPER", "DUPER") == 1
@@ -175,6 +284,15 @@ def verify():
 	assert levenshtein("spam", "ham", w_delete=10, w_replace=2) == 12
 	assert levenshtein("xyz", "abcde", w_insert=0, w_delete=0, w_replace=0) == 0
 	print "No errors with Levenshtein edit distance."
+	print "========================================================"
+	print "Verifying custom Levenshtein edit distance:"
+	assert levenshtein_custom_weights("ANT", "AUNT") == 1
+	assert levenshtein_custom_weights("ANT", "AUNT", delete_map={"A": 10}) == 1
+	assert levenshtein_custom_weights("ANT", "AUNT", insert_map={"U": 10}, w_replace=20) == 10
+	assert levenshtein_custom_weights("AAA", "BB", replace_map={("A", "B"): 0.25}) == 1.5
+	assert levenshtein_custom_weights("AA", "BBB", replace_map={("A", "B"): 0.25}, w_insert=0.5) == 1
+	assert levenshtein_custom_weights("AAA", "BBB", replace_map={("A", "B"): 0.5}) == 1.5
+	print "No errors with custom Levenshtein edit distance."
 
 
 
