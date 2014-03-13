48b74adc05b1d4cabbadbfa6eabdbb94f44851bb
Attila Zseder <zseder@gmail.com>
2014-03-13 11:29:35 +0100

    modified:   close_words.py - given max edit distance (slow except for 1)

--- hunmisc/xstring/close_words.py
+++ hunmisc/xstring/close_words.py
@@ -30,6 +30,8 @@ def read_matrix(istream):
     for l in istream:
         le = l.split("\t")
         src, tgt, w = le
+        if len(src) > 1 or len(tgt) > 1:
+            continue
         w = abs(float(w))
         d[src][tgt] = w
 
@@ -37,9 +39,10 @@ def read_matrix(istream):
 
 
 class CloseWordsGenerator(object):
-    def __init__(self, correct_words, transmatrix=None):
+    def __init__(self, correct_words, transmatrix=None, max_distance=2):
         self.__store_matrix(transmatrix)
         self.corrects = set(tuple(c for c in w) for w in correct_words)
+        self.max_dist = max_distance
 
     def __store_matrix(self, m):
         """stores every row of the matrix in weighted order
@@ -69,7 +72,7 @@ class CloseWordsGenerator(object):
                 result[0] = weight
                 result[1] |= words
                 for w in words:
-                    self.seen[w] = weight
+                    self.seen[w] = (weight, self.seen[word][1] + 1)
 
         return result
 
@@ -87,13 +90,23 @@ class CloseWordsGenerator(object):
         return best
 
     def __get_closest_for_seen(self):
-        best = [None, []]
+        best = [None, set()]
         while len(best[1]) == 0:
-            word, old_weight = sorted(self.seen.iteritems(),
-                                      key=lambda x: x[1])[self.skip_first_n]
-            old_weight = self.seen[word]
+            # stop if there is no possible changes
+            if self.skip_first_n == len(self.seen):
+                break
+
+            word, (old_weight, old_dist) = sorted(self.seen.iteritems(),
+                                      key=lambda x: x[1][1])[self.skip_first_n]
+            print word
+
+            # skip if old_word is already as far as it can be
+            if old_dist == self.max_dist:
+                self.skip_first_n += 1
+                continue
+
             change_weight, new_words = self.get_closest(word)
-            
+
             # @skip_first_n th word is done
             if len(new_words) == 0:
                 self.skip_first_n += 1
@@ -101,28 +114,31 @@ class CloseWordsGenerator(object):
 
             new_weight = old_weight + change_weight
             if best[0] is None:
-                best[0] = new_weight
+                best[0] = (new_weight, old_dist + 1)
                 best[1] = new_words
-            elif new_weight < best[0]:
-                best[0] = new_weight
+            elif new_weight < best[0][0]:
+                best[0] = (new_weight, old_dist + 1)
                 best[1] = new_words
-            elif new_weight == best[0]:
+            elif new_weight == best[0][0]:
                 best[1] |= new_words
         return best
 
     def get_closest_correct(self, word):
         word = tuple(c for c in word)
-        self.seen = {word: 0.}
+        self.seen = {word: (0., 0)}
         self.skip_first_n = 0
         while True:
-            new_weight, new_words = self.__get_closest_for_seen()
+            new_value, new_words = self.__get_closest_for_seen()
+            if len(new_words) == 0:
+                return None
+
             correct_words = new_words & self.corrects
             if len(correct_words) > 0:
                 return correct_words
 
             for w in new_words:
                 if w not in self.seen:
-                    self.seen[w] = new_weight
+                    self.seen[w] = new_value
 
 
 def main():
@@ -130,7 +146,7 @@ def main():
     m = read_matrix(matrix_f)
     good_words = ["facebook", "britney"]
     cwg = CloseWordsGenerator(good_words, m)
-    tests = ["facebok", "fbok", "b"]
+    tests = ["facebok", "facbok"]
     for w in tests:
         print w, cwg.get_closest_correct(w)
 
