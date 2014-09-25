d0ab41a56dfb3e1d793af66138345bb40f01bf8d
Gabor Recski <recski@sztaki.hu>
2014-09-25 13:11:33 +0200

    - added hunmisc.utils as a package - PEP8 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -7,24 +7,27 @@ class HunmiscInstall(install):
     def run(self):
         install.run(self)
         cwd = self.install_libbase
-        call(['cd {0}/hunmisc/liblinear; bash install.sh'.format(cwd)], shell=True)
+        call(['cd {0}/hunmisc/liblinear; bash install.sh'.format(
+            cwd)], shell=True)
 
 def my_setup():
     return setup(
-    author='Attila Zseder',
-    author_email='zseder@gmail.com',
-    name='hunmisc',
-    provides=['hunmisc'],
-    url='https://github.com/zseder/hunmisc',
-    packages=['hunmisc', 'hunmisc.db', 'hunmisc.liblinear', 'hunmisc.xstring', 'hunmisc.spell_checker'],
-    package_data={"hunmisc": ["liblinear/install.sh", "liblinear/liblinear.patch"]},
-    package_dir={'': '.'},
-    include_package_data=True,
-    zip_safe=False,
-    platforms='any',
-    install_requires=["DAWG"],
-    cmdclass={'install': HunmiscInstall}
-)
+        author='Attila Zseder',
+        author_email='zseder@gmail.com',
+        name='hunmisc',
+        provides=['hunmisc'],
+        url='https://github.com/zseder/hunmisc',
+        packages=[
+            'hunmisc', 'hunmisc.db', 'hunmisc.liblinear', 'hunmisc.xstring',
+            'hunmisc.spell_checker', 'hunmisc.utils'],
+        package_data={
+            "hunmisc": ["liblinear/install.sh", "liblinear/liblinear.patch"]},
+        package_dir={'': '.'},
+        include_package_data=True,
+        zip_safe=False,
+        platforms='any',
+        install_requires=["DAWG"],
+        cmdclass={'install': HunmiscInstall})
 
 if __name__ == "__main__":
     my_setup()
