7d8c444c8f1fbf0cd20bf1e23a6e9038b8bb7385
Attila Zséder <zseder@gmail.com>
2014-11-20 17:16:42 +0100

    human_format() added

--- hunmisc/xstring/xstring.py
+++ hunmisc/xstring/xstring.py
@@ -110,3 +110,10 @@ def remove_unwanted_characters_from_word(token, unwanted_set, remove_set=None):
 
         return ret
 
+def human_format(num):
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    # add more suffixes if you need them
+    return '%.1f%s' % (num, ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
