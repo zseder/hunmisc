92c973be47fa3052e371fe8205212fa3132696a0
Gabor Recski <recski@mokk.bme.hu>
2016-02-16 13:34:49 +0100

    PEP8

--- hunmisc/corpustools/tsv_tools.py
+++ hunmisc/corpustools/tsv_tools.py
@@ -23,7 +23,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 """Miscellaneous tools for handling TSV formatted corpora.
 Most functions assume that the first two fields of a file
-correspond to word form and POS-tag/morph-analysis respectively, while the last column is mostly expected to contain sequential tags such as chunk or NER tags."""
+correspond to word form and POS-tag/morph-analysis respectively, while the last
+column is mostly expected to contain sequential tags such as chunk or NER tags.
+"""
 import re
 class InvalidTaggingError(Exception):
     pass
@@ -35,23 +37,23 @@ def sentence_iterator(input):
         line = input.readline()
         if not line:
             break
-        if line == '\n': 
-            if curr_sen==[]:
-                #consecutive empty lines between sentences are tolerated
+        if line == '\n':
+            if curr_sen == []:
+                # consecutive empty lines between sentences are tolerated
                 continue
             yield curr_sen
             curr_sen = []
             continue
         curr_sen.append(line.strip().split('\t'))
-    if curr_sen!=[]:
+    if curr_sen != []:
         yield curr_sen
 
 cas_patt = re.compile('<CAS<...>>')
 def get_np_case(chunk):
     nouns = [tok for tok in chunk if 'NOUN' in tok[1]]
     if nouns == []:
-        #If an NP contains no noun, we assume its rightmost element
-        #to be the head
+        # If an NP contains no noun, we assume its rightmost element
+        # to be the head
         head_kr = chunk[-1][1]
     else:
         head_kr = nouns[-1][1]
@@ -87,7 +89,7 @@ def collapse_chunks(sen):
             new_sen.append(chunk[0])
         else:
             chunk_cat = chunk[0][-1].split('-')[-1]
-            chunk_cat = chunk_cat.split('<')[0]#some datasets already have case
+            chunk_cat = chunk_cat.split('<')[0]  # some datasets already have case # nopep8
             chunk_text = '_'.join([tok[0] for tok in chunk])
             cased_chunk_tag = get_chunk_case(chunk, chunk_cat)
             new_sen.append([chunk_text, cased_chunk_tag, cased_chunk_tag])
@@ -99,17 +101,17 @@ def toks_to_chunks(sen, strict=False):
     tagging must conform to BIE1 standards, otherwise an exception is raised"""
     new_sen = []
     curr_chunk = []
-    #print sen
+    # print sen
     for tok in sen:
-        #print tok
+        # print tok
         if tok[-1] == 'O':
-            if curr_chunk!=[] and strict:
+            if curr_chunk != [] and strict:
                 raise InvalidTaggingError
             new_sen.append([tok])
             continue
         chunk_part, chunk_type = tok[-1].split('-')
         if chunk_part in ('B', '1'):
-            if curr_chunk!=[] and strict:
+            if curr_chunk != [] and strict:
                 raise InvalidTaggingError
         else:
             if curr_chunk == [] and strict:
@@ -120,10 +122,10 @@ def toks_to_chunks(sen, strict=False):
             new_sen.append(curr_chunk)
             curr_chunk = []
 
-    if curr_chunk!=[] and strict:
+    if curr_chunk != [] and strict:
         raise InvalidTaggingError
-            
-    return new_sen    
+
+    return new_sen
 
 def print_sen(sen):
     for tok in sen:
@@ -133,3 +135,7 @@ def print_sen(sen):
 def collapse_chunks_in_corp(stream):
     for sen in sentence_iterator(stream):
         print_sen(collapse_chunks(sen))
+
+def get_dependencies(sen, tok_field=1, lemma_field=2, msd_field=3,
+                     gov_field=-2, dep_field=-1):
+    pass
